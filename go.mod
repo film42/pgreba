4183d39bdcb9e756ceff5f73b1aff07a8df78210
Garrett Thornburg <film42@gmail.com>
2020-06-24 08:22:49 -0700

    Add first working healthcheck

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/film42/pgreplbot
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.7.0
+	google.golang.org/appengine v1.6.6 // indirect
+	gopkg.in/volatiletech/null.v6 v6.0.0-20170828023728-0bef4e07ae1b
+)
--- main.go
+++ main.go
@@ -1,26 +1,95 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
+
 	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	"gopkg.in/volatiletech/null.v6"
 )
 
-func getSlotHealthCheck(w http.ResponseWriter, r *http.Request) {
+type pgReplicationSlot struct {
+	SlotName          string      `db:"slot_name"`
+	Plugin            string      `db:"plugin"`
+	SlotType          string      `db:"slot_type"`
+	Datoid            string      `db:"datoid"`
+	Database          string      `db:"database"`
+	Temporary         bool        `db:"temporary"`
+	Active            bool        `db:"active"`
+	ActivePid         null.String `db:"active_pid"`
+	Xmin              null.String `db:"xmin"`
+	CatalogXmin       string      `db:"catalog_xmin"`
+	RestartLsn        string      `db:"restart_lsn"`
+	ConfirmedFlushLsn string      `db:"confirmed_flush_lsn"`
+}
+
+type HealthChecker struct {
+	DB *sqlx.DB
+}
+
+func (hc *HealthChecker) getSlotByName(name string) (*pgReplicationSlot, error) {
+	// Get all replication slots
+	slots := []*pgReplicationSlot{}
+	err := hc.DB.Select(&slots, "select * from pg_replication_slots limit 1")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, slot := range slots {
+		if slot.SlotName == name {
+			return slot, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func (hc *HealthChecker) getSlotHealthCheck(w http.ResponseWriter, r *http.Request) {
+	// Get request info
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	slotName := params["slot_name"]
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"status": "ok",
+
+	// Get slot
+	slot, err := hc.getSlotByName(slotName)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("ERR:", err)
+		return
+	}
+
+	// If slot is missing return 404
+	if slot == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// If slot is not active return 503
+	if !slot.Active {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}
+
+	// Render the response
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"active": slot.Active,
 		"slot":   slotName,
 	})
 }
 
 func main() {
 	fmt.Println("Hello")
+	db, err := sqlx.Connect("postgres", "host=localhost database=postgres user=postgres sslmode=disable binary_parameters=yes")
+	if err != nil {
+		panic(err)
+	}
+
+	hc := &HealthChecker{DB: db}
+
 	router := mux.NewRouter()
-	router.HandleFunc("/slot/{slot_name}/health_check", getSlotHealthCheck).Methods("GET")
+	router.HandleFunc("/slot/{slot_name}/health_check", hc.getSlotHealthCheck).Methods("GET")
 	http.ListenAndServe(":8000", router)
 }
