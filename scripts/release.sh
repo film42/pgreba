cc023df9c4bbcad470d300a9b7ac3172ce1fb3ae
Garrett Thornburg <film42@gmail.com>
2020-11-13 09:47:23 -0800

    Disable cgo

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,9 @@ set -x
 
 rm -rf pkg/
 
-GOOS=darwin GOARCH=amd64 go build -o pkg/pgreba-darwin-amd64 
-GOOS=linux GOARCH=amd64 go build -o pkg/pgreba-linux-amd64 
-GOOS=linux GOARCH=386 go build -o pkg/pgreba-linux-386 
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o pkg/pgreba-darwin-amd64 
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o pkg/pgreba-linux-amd64 
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o pkg/pgreba-linux-386 
 
 pushd pkg
   for file in $(ls -1 .); do
